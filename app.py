d8b396c881ea4f18673d0d9a5b821e19608e6721
Zulfah Fauziah <zulfah.fauziah.tik18@mhsw.pnj.ac.id>
2022-10-20 15:51:09 +0700

    [ADD] login

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for, redirect, session
 from pymongo import MongoClient
+import bcrypt
+
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.1wag9ha.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+records = db.users
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['post', 'get'])
 def home():
-   return render_template('index.html')
+    message = ''
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+    if request.method == "POST":
+        user = request.form.get("fullname")
+        email = request.form.get("email")
+        
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        user_found = records.find_one({"name": user})
+        email_found = records.find_one({"email": email})
+        if user_found:
+            message = 'There already is a user by that name'
+            return render_template('index.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('index.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('index.html', message=message)
+        else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'name': user, 'email': email, 'password': hashed}
+            records.insert_one(user_input)
+            
+            user_data = records.find_one({"email": email})
+            new_email = user_data['email']
+   
+            return render_template('logged_in.html', email=new_email)
+    return render_template('index.html')
 
 @app.route("/SaveTarget", methods=["POST"])
 def save_target():
